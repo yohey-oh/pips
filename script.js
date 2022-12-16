cde29072c1a8f3b2ae9b815b084b3b281d944371
yohey oomori <link.yohey@gmail.com>
2022-12-16 17:30:55 +0900

    finish プロトタイプ完成

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(function() {
-  
+  $("#button").on('click', function() {
+    const textLOT      = $("#lot option:selected").val();
+    const textPIP      = $("#pip option:selected").val();
+    const textLEVERAGE = $("#leverage").val();
+    var   textNlot     = $("#Nlot").val();
+    var   textNpips    = $("#Npips").val();
+
+    const LOT        = Number(textLOT);
+    const PIP        = Number(textPIP);
+    const LEVERAGE   = Number(textLEVERAGE);
+    var   Nlot       = Number(textNlot);
+    var   Npips      = Number(textNpips);
+    var   ProfitLoss = 0;
+
+    ProfitLoss = LOT * Nlot * PIP * Npips;
+    $("#ProfitLoss").html(ProfitLoss);
+  });
 });
